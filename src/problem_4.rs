689fa8d1785d4a8da668c2338cf133cd04c62d3d
Jacob <JacobEvans.7@outlook.com>
2019-06-09 03:40:29 +0800

    add documentation comments to problem 4 "Median of Two Sorted Arrays"

--- src/problem_4.rs
+++ src/problem_4.rs
@@ -1,25 +1,26 @@
-/*
-There are two sorted arrays nums1 and nums2 of size m and n respectively.
+//! Median of Two Sorted Arrays [link](https://leetcode.com/problems/median-of-two-sorted-arrays/)
+//!
+//! There are two sorted arrays nums1 and nums2 of size m and n respectively.
+//!
+//! Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+//!
+//! You may assume nums1 and nums2 cannot be both empty.
+//!
+//! **Example 1:**
+//!
+//! nums1 = [1, 3]
+//! nums2 = [2]
+//!
+//! The median is 2.0
+//!
+//! **Example 2:**
+//!
+//! nums1 = [1, 2]
+//! nums2 = [3, 4]
+//!
+//! The median is (2 + 3)/2 = 2.5
 
-Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
-
-You may assume nums1 and nums2 cannot be both empty.
-
-Example 1:
-
-nums1 = [1, 3]
-nums2 = [2]
-
-The median is 2.0
-Example 2:
-
-nums1 = [1, 2]
-nums2 = [3, 4]
-
-The median is (2 + 3)/2 = 2.5
-*/
-
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
