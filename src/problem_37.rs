d6899b213c84bef5c0d37632fa1fdfce08f32745
Jacob <JacobEvans.7@outlook.com>
2019-06-10 01:01:01 +0800

    add documentation comments to problem 37 "Sudoku Solver"

--- src/problem_37.rs
+++ src/problem_37.rs
@@ -1,26 +1,48 @@
-/*
-Write a program to solve a Sudoku puzzle by filling the empty cells.
+//! Sudoku Solver [link](https://leetcode.com/problems/sudoku-solver/)
+//!
+//! Write a program to solve a Sudoku puzzle by filling the empty cells.
+//!
+//! A sudoku solution must satisfy **all of the following rules**:
+//!
+//! 1. Each of the digits `1-9` must occur exactly once in each row.
+//! 2. Each of the digits `1-9` must occur exactly once in each column.
+//! 3. Each of the the digits `1-9` must occur exactly once in each of the 9 `3x3` sub-boxes of the grid.
+//!
+//! Empty cells are indicated by the character `'.'`.
+//!
+//! **Example:**
+//! ```
+//! input = vec![
+//!     vec!['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+//!     vec!['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+//!     vec!['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+//!     vec!['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+//!     vec!['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+//!     vec!['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+//!     vec!['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+//!     vec!['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+//!     vec!['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+//! ];
+//! output = vec![
+//!     vec!['5', '3', '4', '6', '7', '8', '9', '1', '2'],
+//!     vec!['6', '7', '2', '1', '9', '5', '3', '4', '8'],
+//!     vec!['1', '9', '8', '3', '4', '2', '5', '6', '7'],
+//!     vec!['8', '5', '9', '7', '6', '1', '4', '2', '3'],
+//!     vec!['4', '2', '6', '8', '5', '3', '7', '9', '1'],
+//!     vec!['7', '1', '3', '9', '2', '4', '8', '5', '6'],
+//!     vec!['9', '6', '1', '5', '3', '7', '2', '8', '4'],
+//!     vec!['2', '8', '7', '4', '1', '9', '6', '3', '5'],
+//!     vec!['3', '4', '5', '2', '8', '6', '1', '7', '9'],
+//! ];
+//! ```
+//!
+//! **Note:**
+//!
+//! - The given board contain only digits `1-9` and the character `'.'`.
+//! - You may assume that the given Sudoku puzzle will have a single unique solution.
+//! - The given board size is always `9x9`.
 
-A sudoku solution must satisfy all of the following rules:
-
-Each of the digits 1-9 must occur exactly once in each row.
-Each of the digits 1-9 must occur exactly once in each column.
-Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
-Empty cells are indicated by the character '.'.
-
-
-A sudoku puzzle...
-
-
-...and its solution numbers marked in red.
-
-Note:
-
-The given board contain only digits 1-9 and the character '.'.
-You may assume that the given Sudoku puzzle will have a single unique solution.
-The given board size is always 9x9.
-*/
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
