e7a6d2b141e8e9f072ae46b6bcaae2ea2617db58
Jacob <JacobEvans.7@outlook.com>
2019-06-09 18:23:22 +0800

    add documentation comments to problem 10 "Regular Expression Matching"

--- src/problem_10.rs
+++ src/problem_10.rs
@@ -1,51 +1,77 @@
-/*
-Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+//! Regular Expression Matching [link](https://leetcode.com/problems/regular-expression-matching/)
+//!
+//! Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+//!
+//! '.' Matches any single character.
+//!
+//! '*' Matches zero or more of the preceding element.
+//!
+//! The matching should cover the entire input string (not partial).
+//!
+//! **Note:**
+//!
+//! - s could be empty and contains only lowercase letters a-z.
+//! - p could be empty and contains only lowercase letters a-z, and characters like . or *.
+//!
+//! **Example 1:**
+//!
+//! *Input:*
+//!
+//! s = "aa"
+//!
+//! p = "a"
+//!
+//! *Output:* false
+//!
+//! *Explanation:* "a" does not match the entire string "aa".
+//!
+//! **Example 2:**
+//!
+//! *Input:*
+//!
+//! s = "aa"
+//!
+//! p = "a*"
+//!
+//! *Output:* true
+//!
+//! *Explanation:* '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+//!
+//! **Example 3:**
+//!
+//! *Input:*
+//!
+//! s = "ab"
+//!
+//! p = ".*"
+//!
+//! *Output:* true
+//!
+//! *Explanation*: ".*" means "zero or more (*) of any character (.)".
+//!
+//! **Example 4:**
+//!
+//! *Input:*
+//!
+//! s = "aab"
+//!
+//! p = "c*a*b"
+//!
+//! *Output:* true
+//!
+//! *Explanation:* c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+//!
+//! **Example 5:**
+//!
+//! *Input:*
+//!
+//! s = "mississippi"
+//!
+//! p = "mis*is*p*."
+//!
+//! *Output:* false
 
-'.' Matches any single character.
-'*' Matches zero or more of the preceding element.
-The matching should cover the entire input string (not partial).
-
-Note:
-
-s could be empty and contains only lowercase letters a-z.
-p could be empty and contains only lowercase letters a-z, and characters like . or *.
-Example 1:
-
-Input:
-s = "aa"
-p = "a"
-Output: false
-Explanation: "a" does not match the entire string "aa".
-Example 2:
-
-Input:
-s = "aa"
-p = "a*"
-Output: true
-Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
-Example 3:
-
-Input:
-s = "ab"
-p = ".*"
-Output: true
-Explanation: ".*" means "zero or more (*) of any character (.)".
-Example 4:
-
-Input:
-s = "aab"
-p = "c*a*b"
-Output: true
-Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
-Example 5:
-
-Input:
-s = "mississippi"
-p = "mis*is*p*."
-Output: false
-*/
-
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
@@ -96,6 +122,7 @@ mod test {
             true
         );
     }
+
     #[test]
     fn example_5() {
         assert_eq!(
