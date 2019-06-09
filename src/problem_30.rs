ff9931aff7bcfa4207ffd5c8b822febab414907c
Jacob <JacobEvans.7@outlook.com>
2019-06-09 19:21:27 +0800

    add documentation comments to problem 30 "Substring with Concatenation of All Words"

--- src/problem_30.rs
+++ src/problem_30.rs
@@ -1,22 +1,26 @@
-/*
-You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
-
-Example 1:
-
-Input:
-  s = "barfoothefoobarman",
-  words = ["foo","bar"]
-Output: [0,9]
-Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
-The output order does not matter, returning [9,0] is fine too.
-Example 2:
-
-Input:
-  s = "wordgoodgoodgoodbestword",
-  words = ["word","good","best","word"]
-Output: []
-*/
-
+//! Substring with Concatenation of All Words [link](https://leetcode.com/problems/substring-with-concatenation-of-all-words/)
+//!
+//! You are given a string, **s**, and a list of words, **words**, that are all of the same length. Find all starting indices of substring(s) in **s** that is a concatenation of each word in **words** exactly once and without any intervening characters.
+//!
+//! **Example 1:**
+//!
+//! ```plain
+//! Input:
+//!   s = "barfoothefoobarman",
+//!   words = ["foo","bar"]
+//! Output: [0,9]
+//! Explanation: Substrings starting at index 0 and 9 are "barfoor" and "foobar" respectively.
+//! The output order does not matter, returning [9,0] is fine too.
+//! ```
+//!
+//! **Example 2:**
+//!
+//! ```plain
+//! Input:
+//!   s = "wordgoodgoodgoodbestword",
+//!   words = ["word","good","best","word"]
+//! Output: []
+//! ```
 use std::collections::HashMap;
 
 struct Solution;
