a91b279009a18fdc80150a616d0178513eb8e19a
Jacob <JacobEvans.7@outlook.com>
2019-06-10 00:49:31 +0800

    add documentation comments to problem 32 "Longest Valid Parentheses"

--- src/problem_32.rs
+++ src/problem_32.rs
@@ -1,19 +1,21 @@
-/*
-Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+//! Longest Valid Parentheses [link](https://leetcode.com/problems/longest-valid-parentheses/)
+//!
+//! Given a string containing just the characters `'('` and `')'`, find the length of the longest valid (well-formed) parentheses substring.
+//!
+//! **Example 1:**
+//! ```plain
+//! Input: "(()"
+//! Output: 2
+//! Explanation: The longest valid parentheses substring is "()"
+//! ```
+//! **Example 2:**
+//! ```plain
+//! Input: ")()())"
+//! Output: 4
+//! Explanation: The longest valid parentheses substring is "()()"
+//! ```
 
-Example 1:
-
-Input: "(()"
-Output: 2
-Explanation: The longest valid parentheses substring is "()"
-Example 2:
-
-Input: ")()())"
-Output: 4
-Explanation: The longest valid parentheses substring is "()()"
-*/
-
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn longest_valid_parentheses(s: String) -> i32 {
