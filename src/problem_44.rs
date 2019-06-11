01333539b240ddb04c39f279dddb927b49f7c076
Jacob <JacobEvans.7@outlook.com>
2019-06-12 00:04:44 +0800

    add documentation comments to problem 44 "Wildcard Matching"

--- src/problem_44.rs
+++ src/problem_44.rs
@@ -1,51 +1,62 @@
-/*
-Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+//! Wildcard Matching [link](https://leetcode.com/problems/wildcard-matching/)
+//!
+//! Given an input string (`s`) and a pattern (`p`), implement wildcard pattern matching with support for `'?'` and `'*'`.
+//! ```plain
+//! '?' Matches any single character.
+//! '*' Matches any sequence of characters (including the empty sequence).
+//! ```
+//! The matching should cover the **entire** input string (not partial).
+//!
+//! **Note:**
+//!
+//! - `s` could be empty and contains only lowercase letters `a-z`.
+//! - `p` could be empty and contains only lowercase letters `a-z`, and characters like `?` or `*`.
+//!
+//! **Example 1:**
+//! ```plain
+//! Input:
+//! s = "aa"
+//! p = "a"
+//! Output: false
+//! Explanation: "a" does not match the entire string "aa".
+//! ```
+//!
+//! **Example 2:**
+//! ```plain
+//! Input:
+//! s = "aa"
+//! p = "*"
+//! Output: true
+//! Explanation: '*' matches any sequence.
+//! ```
+//!
+//! **Example 3:**
+//! ```plain
+//! Input:
+//! s = "cb"
+//! p = "?a"
+//! Output: false
+//! Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+//! ```
+//!
+//! **Example 4:**
+//! ```plain
+//! Input:
+//! s = "adceb"
+//! p = "*a*b"
+//! Output: true
+//! Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+//! ```
+//!
+//! **Example 5:**
+//! ```plain
+//! Input:
+//! s = "acdcb"
+//! p = "a*c?b"
+//! Output: false
+//! ```
 
-'?' Matches any single character.
-'*' Matches any sequence of characters (including the empty sequence).
-The matching should cover the entire input string (not partial).
-
-Note:
-
-s could be empty and contains only lowercase letters a-z.
-p could be empty and contains only lowercase letters a-z, and characters like ? or *.
-Example 1:
-
-Input:
-s = "aa"
-p = "a"
-Output: false
-Explanation: "a" does not match the entire string "aa".
-Example 2:
-
-Input:
-s = "aa"
-p = "*"
-Output: true
-Explanation: '*' matches any sequence.
-Example 3:
-
-Input:
-s = "cb"
-p = "?a"
-Output: false
-Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
-Example 4:
-
-Input:
-s = "adceb"
-p = "*a*b"
-Output: true
-Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
-Example 5:
-
-Input:
-s = "acdcb"
-p = "a*c?b"
-Output: false
-*/
-
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
