1ff8c1218c7db76e97dd1df3bab62ba692454f10
Jacob <JacobEvans.7@outlook.com>
2019-06-13 23:38:25 +0800

    add documentation comments to problem 45 "Jump Game II"

--- src/problem_45.rs
+++ src/problem_45.rs
@@ -1,21 +1,24 @@
-/*
-Given an array of non-negative integers, you are initially positioned at the first index of the array.
+//! Jump Game II [link](https://leetcode.com/problems/jump-game-ii/)
+//!
+//! Given an array of non-negative integers, you are initially positioned at the first index of the array.
+//!
+//! Each element in the array represents your maximum jump length at that position.
+//!
+//! Your goal is to reach the last index in the minimum number of jumps.
+//!
+//! **Example:**
+//! ```plain
+//! Input: [2,3,1,1,4]
+//! Output: 2
+//! Explanation: The minimum number of jumps to reach the last index is 2.
+//!     Jump 1 step from index 0 to 1, then 3 steps to the last index.
+//! ```
+//!
+//! **Note:**
+//!
+//! You can assume that you can always reach the last index.
 
-Each element in the array represents your maximum jump length at that position.
-
-Your goal is to reach the last index in the minimum number of jumps.
-
-Example:
-
-Input: [2,3,1,1,4]
-Output: 2
-Explanation: The minimum number of jumps to reach the last index is 2.
-    Jump 1 step from index 0 to 1, then 3 steps to the last index.
-Note:
-
-You can assume that you can always reach the last index.
-*/
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn jump(nums: Vec<i32>) -> i32 {
