f596153b18155a27b8e7636a033213dc4c8eddc3
Jacob <JacobEvans.7@outlook.com>
2019-06-15 23:51:11 +0800

    add documentation comments to problem 51 "N-Queens"

--- src/problem_51.rs
+++ src/problem_51.rs
@@ -1,27 +1,29 @@
-/*
-The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other.
+//! N-Queens [link](https://leetcode.com/problems/n-queens/)
+//!
+//! The *n*-queens puzzle is the problem of placing *n* queens on an *n*×*n* chessboard such that no two queens attack each other.
+//!
+//! Given an integer *n*, return all distinct solutions to the *n*-queens puzzle.
+//!
+//! Each solution contains a distinct board configuration of the *n*-queens' placement, where `'Q'` and `'.'` both indicate a queen and an empty space respectively.
+//!
+//! **Example:**
+//! ```plain
+//! Input: 4
+//! Output: [
+//!  [".Q..",  // Solution 1
+//!   "...Q",
+//!   "Q...",
+//!   "..Q."],
+//!
+//!  ["..Q.",  // Solution 2
+//!   "Q...",
+//!   "...Q",
+//!   ".Q.."]
+//! ]
+//! Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
+//! ```
 
-Given an integer n, return all distinct solutions to the n-queens puzzle.
-
-Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.
-
-Example:
-
-Input: 4
-Output: [
- [".Q..",  // Solution 1
-  "...Q",
-  "Q...",
-  "..Q."],
-
- ["..Q.",  // Solution 2
-  "Q...",
-  "...Q",
-  ".Q.."]
-]
-Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above.
-*/
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
