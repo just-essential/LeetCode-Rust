54ab1e261022678b1c29976545431e197816e8f7
Jacob <JacobEvans.7@outlook.com>
2019-06-22 00:02:21 +0800

    add documentation comments to problem 68 "Text Justification"

--- src/problem_68.rs
+++ src/problem_68.rs
@@ -11,8 +11,8 @@
 //! **Note:**
 //!
 //! - A word is defined as a character sequence consisting of non-space characters only.
-//! - Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
-//! - The input array words contains at least one word.
+//! - Each word's length is guaranteed to be greater than 0 and not exceed *maxWidth*.
+//! - The input array `words` contains at least one word.
 //!
 //! **Example 1:**
 //! ```plain
