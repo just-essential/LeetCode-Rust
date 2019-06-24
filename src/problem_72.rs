2adfd0a43ecee6f293a0e5ec21465b41c0b9cf93
Jacob <JacobEvans.7@outlook.com>
2019-06-24 13:22:06 +0800

    add documentation comments to problem 72 "Edit Distance"

--- src/problem_72.rs
+++ src/problem_72.rs
@@ -1,33 +1,38 @@
-/*
-Given two words word1 and word2, find the minimum number of operations required to convert word1 to word2.
+//! Edit Distance [link](https://leetcode.com/problems/edit-distance/)
+//!
+//! Given two words *word1* and *word2*, find the minimum number of operations required to convert *word1* to *word2*.
+//!
+//! You have the following 3 operations permitted on a word:
+//!
+//! 1. Insert a character
+//! 2. Delete a character
+//! 3. Replace a character
+//!
+//! **Example 1:**
+//! ```plain
+//! Input: word1 = "horse", word2 = "ros"
+//! Output: 3
+//! Explanation:
+//! horse -> rorse (replace 'h' with 'r')
+//! rorse -> rose (remove 'r')
+//! rose -> ros (remove 'e')
+//! ```
+//!
+//! **Example 2:**
+//! ```plain
+//! Input: word1 = "intention", word2 = "execution"
+//! Output: 5
+//! Explanation:
+//! intention -> inention (remove 't')
+//! inention -> enention (replace 'i' with 'e')
+//! enention -> exention (replace 'n' with 'x')
+//! exention -> exection (replace 'n' with 'c')
+//! exection -> execution (insert 'u')
+//! ```
 
-You have the following 3 operations permitted on a word:
-
-Insert a character
-Delete a character
-Replace a character
-Example 1:
-
-Input: word1 = "horse", word2 = "ros"
-Output: 3
-Explanation:
-horse -> rorse (replace 'h' with 'r')
-rorse -> rose (remove 'r')
-rose -> ros (remove 'e')
-Example 2:
-
-Input: word1 = "intention", word2 = "execution"
-Output: 5
-Explanation:
-intention -> inention (remove 't')
-inention -> enention (replace 'i' with 'e')
-enention -> exention (replace 'n' with 'x')
-exention -> exection (replace 'n' with 'c')
-exection -> execution (insert 'u')
-*/
 use std::cmp::min;
 
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn min_distance(word1: String, word2: String) -> i32 {
