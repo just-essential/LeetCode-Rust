819e4d938639d23991c462f7781cd4d25722bf9a
Jacob <JacobEvans.7@outlook.com>
2019-06-26 23:44:44 +0800

    add documentation comments to problem 84 "Largest Rectangle in Histogram"

--- src/problem_84.rs
+++ src/problem_84.rs
@@ -1,16 +1,14 @@
-/*
-Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+//! Largest Rectangle in Histogram [link](https://leetcode.com/problems/largest-rectangle-in-histogram/)
+//!
+//! Given *n* non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+//!
+//! **Example:**
+//! ```plain
+//! Input: [2,1,5,6,2,3]
+//! Output: 10
+//! ```
 
-Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
-
-The largest rectangle is shown in the shaded area, which has area = 10 unit.
-
-Example:
-
-Input: [2,1,5,6,2,3]
-Output: 10
-*/
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
