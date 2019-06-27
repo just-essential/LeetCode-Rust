5d5eef96f0fe99363c8fa1b77979e71c2705250c
Jacob <JacobEvans.7@outlook.com>
2019-06-27 23:36:50 +0800

    add documentation comments to problem 85 "Maximal Rectangle"

--- src/problem_85.rs
+++ src/problem_85.rs
@@ -1,18 +1,20 @@
-/*
-Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+//! Maximal Rectangle [link](https://leetcode.com/problems/maximal-rectangle/)
+//!
+//! Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+//!
+//! **Example:**
+//! ```plain
+//! Input:
+//! [
+//!   ["1","0","1","0","0"],
+//!   ["1","0","1","1","1"],
+//!   ["1","1","1","1","1"],
+//!   ["1","0","0","1","0"]
+//! ]
+//! Output: 6
+//! ```
 
-Example:
-
-Input:
-[
-  ["1","0","1","0","0"],
-  ["1","0","1","1","1"],
-  ["1","1","1","1","1"],
-  ["1","0","0","1","0"]
-]
-Output: 6
-*/
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn maximal_rectangle(matrix: Vec<Vec<char>>) -> i32 {
