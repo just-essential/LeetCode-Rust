052a4687a1fd2cc5093f5e7b430007a10a463700
Jacob <JacobEvans.7@outlook.com>
2019-07-04 01:15:43 +0800

    add documentation comments to problem 97 "Interleaving String"

--- src/problem_97.rs
+++ src/problem_97.rs
@@ -1,16 +1,20 @@
-/*
-Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+//! Interleaving String [link](https://leetcode.com/problems/interleaving-string/)
+//!
+//! Given *s1*, *s2*, *s3*, find whether s3 is formed by the interleaving of *s1* and *s2*.
+//!
+//! **Example 1:**
+//! ```plain
+//! Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+//! Output: true
+//! ```
+//!
+//! **Example 2:**
+//! ```plain
+//! Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+//! Output: false
+//! ```
 
-Example 1:
-
-Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
-Output: true
-Example 2:
-
-Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
-Output: false
-*/
-struct Solution;
+pub struct Solution;
 
 impl Solution {
     pub fn is_interleave(s1: String, s2: String, s3: String) -> bool {
